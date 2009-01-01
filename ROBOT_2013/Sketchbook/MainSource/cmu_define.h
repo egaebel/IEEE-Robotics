fa3225e363a230e4d473a3b6e1a002bfe68588e1
GateKeeperX <bls220@vt.edu>
2009-01-01 05:03:44 -0500

    Updated all RGB values.

--- ROBOT_2013/Sketchbook/MainSource/cmu_define.h
+++ ROBOT_2013/Sketchbook/MainSource/cmu_define.h
@@ -1,54 +1,53 @@
-#define WHITE_R_MIN 225
+#define WHITE_R_MIN 195
 #define WHITE_R_MAX 255
-#define WHITE_G_MIN 250
-
-#define WHITE_G_MAX 255
-#define WHITE_B_MIN 220
+#define WHITE_G_MIN 195
+#define WHITE_G_MAX 251
+#define WHITE_B_MIN 195
 #define WHITE_B_MAX 255
 
-#define BLUE_R_MIN 120
-#define BLUE_R_MAX 180
-#define BLUE_G_MIN 250
+#define BLUE_R_MIN 100
+#define BLUE_R_MAX 140
+#define BLUE_G_MIN 190
 #define BLUE_G_MAX 255
-#define BLUE_B_MIN 250
+#define BLUE_B_MIN 235
 #define BLUE_B_MAX 255
 
-#define RED_R_MIN 205
+#define RED_R_MIN 200
 #define RED_R_MAX 255
-#define RED_G_MIN 105
-#define RED_G_MAX 120
-#define RED_B_MIN 90
-#define RED_B_MAX 110
+#define RED_G_MIN 90
+#define RED_G_MAX 160
+#define RED_B_MIN 65
+#define RED_B_MAX 160
 
 #define YELLOW_R_MIN 230
 #define YELLOW_R_MAX 255
-#define YELLOW_G_MIN 230
+#define YELLOW_G_MIN 222
 #define YELLOW_G_MAX 255
-#define YELLOW_B_MIN 145
-#define YELLOW_B_MAX 160
+#define YELLOW_B_MIN 172
+#define YELLOW_B_MAX 213
 
-#define GREEN_R_MIN 55
-#define GREEN_R_MAX 75
-#define GREEN_G_MIN 90
-#define GREEN_G_MAX 115
-#define GREEN_B_MIN 70
-#define GREEN_B_MAX 115
+#define GREEN_R_MIN 60
+#define GREEN_R_MAX 120
+#define GREEN_G_MIN 85
+#define GREEN_G_MAX 255
+#define GREEN_B_MIN 92
+#define GREEN_B_MAX 130
 
-#define BROWN_R_MIN 65
-#define BROWN_R_MAX 75
-#define BROWN_G_MIN 65
-#define BROWN_G_MAX 75
-#define BROWN_B_MIN 55
-#define BROWN_B_MAX 70
+#define BROWN_R_MIN 80
+#define BROWN_R_MAX 128
+#define BROWN_G_MIN 76
+#define BROWN_G_MAX 125
+#define BROWN_B_MIN 71
+#define BROWN_B_MAX 102
 
-#define PURPLE_R_MIN 140
-#define PURPLE_R_MAX 220
-#define PURPLE_G_MIN 180
-#define PURPLE_G_MAX 235
-#define PURPLE_B_MIN 230
+#define PURPLE_R_MIN 115
+#define PURPLE_R_MAX 180
+#define PURPLE_G_MIN 150
+#define PURPLE_G_MAX 190
+#define PURPLE_B_MIN 195
 #define PURPLE_B_MAX 255
 
-
-
+#define CAM_BRIGHTNESS  34
+#define CAM_CONTRAST    -3
 
 #define PIXEL_BLOCK_MIN 35 //minimum % of pixels to identify a block
