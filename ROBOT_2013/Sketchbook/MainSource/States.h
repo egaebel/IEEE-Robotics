2a0c7e8e2f1a59c6c5fdf3fdb5867832bcf1b21a
egaebel <egaebel@vt.edu>
2012-10-18 23:14:16 -0400

    added to fix multiple inclusion error, holds all State function prototypes

---
+++ ROBOT_2013/Sketchbook/MainSource/States.h
@@ -0,0 +1,21 @@
+//State Function Prototypes
+void initEnter();
+void initUpdate();
+void initExit();
+
+void scanEnter();
+void scanUpdate();
+void scanExit();
+
+void moveToEnter();
+void moveToUpdate();
+void moveToExit();
+
+void pickUpEnter();
+void pickUpUpdate();
+void pickUpExit();
+
+void dropEnter();
+void dropUpdate();
+void dropExit();
+//End State Function Prototypes
