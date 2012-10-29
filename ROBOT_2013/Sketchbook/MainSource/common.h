26f3c8ec8bcd445b615128414f3844f0b518e5ce
GateKeeperX <bls220@vt.edu>
2012-10-29 02:02:31 -0400

    Added motor related constants and pin designations. ~Ben S

--- ROBOT_2013/Sketchbook/MainSource/common.h
+++ ROBOT_2013/Sketchbook/MainSource/common.h
@@ -6,18 +6,22 @@
 */
 
 enum POSITIONS{	POS_START = 0,
-		POS_SEA_LOAD,
-		POS_RAIL_LOAD,
-		POS_PICK_UP,
-		POS_AIR
-	      };
+				POS_SEA_LOAD,
+				POS_RAIL_LOAD,
+				POS_PICK_UP,
+				POS_AIR
+	      	};
 
-enum COLOR{};
+enum COLOR{}; //TODO: Colors Should be defined.
 
 int curPos = 0;
 int nextPos = 0;
 
 /* PIN DEFINITIONS */
-#define COLOR_SENSOR 0 //Pin for color sensor (pin 0 is fake)
+#define COLOR_SENSOR 0 //Pin for color sensor (pin 0 is fake) 
+	//Motors (Servos)
+#define LEFT_MOTOR 9
+#define RIGHT_MOTOR 10
+#define REAR_MOTOR 11
 
 #endif
