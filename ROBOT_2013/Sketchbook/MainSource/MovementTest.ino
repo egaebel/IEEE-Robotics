73cc1f0185e3ca7e9a1e8daa3813d887a5332ad4
GateKeeperX <bls220@vt.edu>
2012-11-01 21:21:39 -0400

    Added Test Sketch for Movement class. Tested OK! ~Ben S.

---
+++ ROBOT_2013/Sketchbook/MainSource/MovementTest.ino
@@ -0,0 +1,29 @@
+
+//Sketch for testing the movement class
+
+#include <Arduino.h>
+#include <Servo.h>
+#include "common.h"
+#include "Movement.h"
+
+Movement move;
+
+void setup(){
+  move.init();
+}
+
+void loop(){
+  move.forward(1);
+  delay(5000);
+  move.backward(1);
+  delay(5000);
+  move.turnLeft(1);
+  delay(5000);
+  move.turnRight(1);
+  move.stop();
+  delay(5000);
+  move.slideLeft(1);
+  delay(5000);
+  move.slideRight(1);
+  delay(5000);
+}
