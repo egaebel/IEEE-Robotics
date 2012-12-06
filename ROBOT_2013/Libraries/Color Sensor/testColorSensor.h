a422c609923de6000cbaf9066961df89c8d42c87
root <root@adahan-ThinkPad-X220-Tablet.(none)>
2012-12-06 17:25:11 -0500

    testColor sensor used of getting pulse values

---
+++ ROBOT_2013/Libraries/Color Sensor/testColorSensor.h
@@ -0,0 +1,29 @@
+#ifndef _COLORSENSOR_H_
+#define _COLORSENSOR_H_
+/*
+||
+|| @file colorSensor.h
+|| @author VT IEEE Hardware Team
+||
+|| @description
+|| | Provides a abstract method to call in order to get the color of the object in front of the robot. (For TCS3200 Color sensor)
+|| #
+||
+|| @license
+|| | Declares the methods from Eecher's TCS3200 program adapted from code found at reibot.org
+|| #
+||
+*/
+
+//TODO define the functionality of the states
+class ColorSensor{
+	public:
+		void testing();
+	private:
+		void TCS3200setup();
+		int detectColor(int taosOutPin);
+		float colorRead(int taosOutPin, int color, boolean LEDstate);
+		void taosMode(int mode);
+};
+
+#endif
