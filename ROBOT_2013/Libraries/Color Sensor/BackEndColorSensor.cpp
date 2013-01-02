c8e133e218ea6a3b1bfc6452cfb4211898764082
Antoine Dahan <adahan93@gmail.com>
2013-01-02 03:21:12 -0500

    Planned backend files for color sensor

---
+++ ROBOT_2013/Libraries/Color Sensor/BackEndColorSensor.cpp
@@ -0,0 +1,22 @@
+#include "BackEndColorSensor.h"
+using namespace Color;
+BackEndColorSensor::BackEndColorSensor()    {
+
+}
+
+BackEndColorSensor::detectColor(Color color)    {
+
+}
+
+BackEndColorSensor::void TCS3200setup() {
+
+}
+
+BackEndColorSensor::float colorRead(int taosOutPin, int color, boolean LEDstate)    {
+
+}
+
+BackEndColorSensor::void taosMode(int mode) {
+
+}
+
---
+++ ROBOT_2013/Libraries/Color Sensor/BackEndColorSensor.h
@@ -0,0 +1,17 @@
+#ifndef BACKENDCOLORSENSOR_H
+#define BACKENDCOLORSENSOR_H
+
+using namespace Color;
+
+class BackEndColorSensor
+{
+    public:
+        BackEndColorSensor(); //Nescessary color sensor setup called in constructor
+        int detectColor(Color color);
+    private:
+        void TCS3200setup();
+        float colorRead(int taosOutPin, int color, boolean LEDstate);
+        void taosMode(int mode);
+};
+
+#endif // BACKENDCOLORSENSOR_H
