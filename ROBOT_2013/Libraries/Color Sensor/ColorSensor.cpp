48206637de020243a2bb85ed447c8d279042f8c7
root <root@antoine-ThinkPad-X220-Tablet.(none)>
2013-01-24 08:56:57 -0500

    Added ColorSensor class

--- ROBOT_2013/Libraries/Color Sensor/ColorSensor.cpp
+++ ROBOT_2013/Libraries/Color Sensor/ColorSensor.cpp
@@ -1,1 +1,35 @@
+#include "ColorSensor.h"
 
+//Feild Variables
+BackEndColorSensor backEnd;
+int whitePulseValueTotal;
+int bluePulseValueTotal;
+int redPulseValueTotal;
+int greenPulseValueTotal;
+
+ColorSensor::ColorSensor()
+{
+    backEnd = new BackEndColorSensor();
+}
+
+/**
+ *Detects color in front of sensor
+ * Takes pulse readings and stores until numberOfPulseReadings number of pulse readings and then
+ * determines color based on patterns, ratios, or larger values.
+ *@param the number of pulseReadings to take (more = more confidence in result)
+ *@returns the color to in front of the color sensor
+                    0 = white
+                    1 = blue
+                    2 = red
+                    3 = green
+ */
+ColorSensor::detectColor(int numberOfPulseReadings) {
+//TODO Look at results of color sensor pulse readings in the lab
+}
+
+/**
+ * See detectColor(int numberOfPulseReadings) documentation
+ */
+ColorSensor::detectColor()   {
+//TODO Look at results of color sensor pulse readings in the lab
+}
