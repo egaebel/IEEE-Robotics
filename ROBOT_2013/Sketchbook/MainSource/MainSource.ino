b2b0d4d7cc1465d71420b06bc5141a6326ee6c40
Hunter Hyde <hunter@dwinq.com>
2013-01-24 17:49:25 -0500

    Testing Color Sensors

--- ROBOT_2013/Sketchbook/MainSource/MainSource.ino
+++ ROBOT_2013/Sketchbook/MainSource/MainSource.ino
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #include "FiniteStateMachine.h"
 #include "States.h"
-
+//#include "BackEndColorSensor.h"
 extern State initState;
 FiniteStateMachine fsm(initState);
+//BackEndColorSensor colorSensor;
 
 void setup()
 {
-
+  //colorSensor = new BackEndColorSensor();
+  Serial.begin(9600);
 }
 
 void loop()
 {
-	fsm.update();
+        Serial.println("White:  Blue:  Red:  Green:");
+        //Serial.println("%d  %d  %d  %d", colorSensor.readPulse(0), colorSensor.readPulse(1), colorSensor.readPulse(2), colorSensor.readPulse(3));
+	//fsm.update();
 }
