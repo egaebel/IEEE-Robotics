4a6bf9d85b31d20729e040f519fc5671f2f6e0b8
root <root@antoine-ThinkPad-X220-Tablet.(none)>
2013-01-29 00:02:58 -0500

    TODO: Pick Up patterns in pulse readings

---
+++ ROBOT_2013/Libraries/Color Sensor/workingDir/ColorSensor.cpp
@@ -0,0 +1,56 @@
+#include "ColorSensor.h"
+
+//Feild Variables
+BackEndColorSensor backEnd;
+int whitePulseValueTotal;
+int bluePulseValueTotal;
+int redPulseValueTotal;
+int greenPulseValueTotal;
+
+ColorSensor::ColorSensor()
+{
+    backEnd = new BackEndColorSensor();
+}
+
+/**
+ *Detects color in front of sensor
+ * Takes pulse readings and stores until numberOfPulseReadings number of pulse readings and then
+ * determines color based on patterns, ratios, or larger values.
+ *@param the number of pulseReadings to take (more = more confidence in result)
+ *@returns the color to in front of the color sensor
+                    0 = white
+                    1 = blue
+                    2 = red
+                    3 = green
+ */
+ColorSensor::detectColor(int numberOfPulseReadings) {
+
+    for(; numberOfPulseReadings > 0; numberOfPulseReadings--)  {
+        whitePulseValueTotal += backEnd.colorRead(0);
+        bluePulseValueTotal += backEnd.colorRead(1);
+        redPulseValueTotal += backEnd.colorRead(2);
+        greenPulseValueTotal += backEnd.colorRead(3);
+    }
+
+//TODO Look at results of color sensor pulse readings in the lab
+}
+
+/**
+ * See detectColor(int numberOfPulseReadings) documentation
+ */
+ColorSensor::detectColor()   {
+
+    for(int readCounter = 0; readCounter < 5; readCounter++)  {
+        whitePulseValueTotal += backEnd.colorRead(0);
+        bluePulseValueTotal += backEnd.colorRead(1);
+        redPulseValueTotal += backEnd.colorRead(2);
+        greenPulseValueTotal += backEnd.colorRead(3);
+    }
+
+
+
+//TODO Look at results of color sensor pulse readings in the lab
+}
+
+
+
---
+++ ROBOT_2013/Libraries/Color Sensor/workingDir/ColorSensor.h
@@ -0,0 +1,15 @@
+#ifndef COLORSENSOR_H
+#define COLORSENSOR_H
+
+
+class ColorSensor
+{
+    public:
+        ColorSensor();
+        int detectColor(int numberOfPulseReadings);
+        int detectColor();
+    protected:
+    private:
+};
+
+#endif // COLORSENSOR_H
