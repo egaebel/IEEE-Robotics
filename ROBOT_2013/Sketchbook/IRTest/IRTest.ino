8b6f814823a56e36266a46aeaa624792d2f32a37
Scott Sines <ssines@vt.edu>
2013-02-05 17:50:33 -0500

    IR sensor reads distances.

--- ROBOT_2013/Sketchbook/IRTest/IRTest.ino
+++ ROBOT_2013/Sketchbook/IRTest/IRTest.ino
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include "IRlib.h"
 
-IRAverager *ir_sea;
-IRAverager *ir_rail;
+IRAverager ir_sea(1);
+IRAverager ir_rail(0);
 
 int sea;
 int rail;
@@ -11,23 +11,17 @@ void setup()
 {
   	Serial.begin(115200); //Sets the data transfer rate at 115.2 Kbps
 	Serial.print("\n\n\nready\n\n\n\n\n\n");
-  ir_sea = &IRAverager(1);
-  ir_rail= &IRAverager(0);
 }
 
 void loop()
 {
-  
- ir_sea->updateIR();
- ir_rail->updateIR();
- Serial.println(ir_sea->getIR());
- sea = analogRead(1);
- rail = analogRead(0);
- if(rail>480)
+ ir_sea.updateIR();
+ ir_rail.updateIR();
+ 
+ if(ir_rail.getIR()<4.5)
    Serial.println("boom rail block");
- else if(sea>480)
+ else if(ir_sea.getIR()<4.5)
    Serial.println("boom sea block");
   else
     Serial.println("air block!");
-    delay(1000);
 }
