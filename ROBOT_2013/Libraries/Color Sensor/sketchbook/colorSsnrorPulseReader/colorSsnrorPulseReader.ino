4825669322ecd22c4da520efcb17aeb5377b017e
Antoine Dahan <adahan@vt.edu>
2013-02-05 21:15:13 -0500

    Added the pulse reader for taking further values

---
+++ ROBOT_2013/Libraries/Color Sensor/sketchbook/colorSsnrorPulseReader/colorSsnrorPulseReader.ino
@@ -0,0 +1,21 @@
+#include "BackEndColorSensor.h"
+#include "Arduino.h"
+BackEndColorSensor *backEnd;
+
+
+void setup() {
+    backEnd = &BackEndColorSensor();
+}
+
+void loop()	{
+    Serial.begin(115200);
+    Serial.print("\n\n\n\n\n");
+	Serial.print("White");
+	Serial.print(backEnd->colorRead(0));
+        Serial.print("\n Blue");
+        Serial.print(backEnd->colorRead(1));
+        Serial.print("\n Red:");
+        Serial.print(backEnd->colorRead(2));
+        Serial.print("\n Green:");
+        Serial.print(backEnd->colorRead(3));
+}
