fddd4fd52f687bc149106aa3e8870d053f4d21f0
tbporter <travis42@vt.edu>
2013-02-23 16:08:46 -0500

    Added EasyTransfer to IRLineSlave

--- ROBOT_2013/Sketchbook/IRLineSlave/IRLineSlave.ino
+++ ROBOT_2013/Sketchbook/IRLineSlave/IRLineSlave.ino
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 #include "LineSensor.h"
+#include "EasyTransfer.h"
+
 
 LineSensor sensor;
-boolean lineResult[16];
+
+struct DATA_TX_STRUCT{
+   boolean line[16];
+}dataTX;
+
+EasyTransfer ET;
 
 void setup()
 {
   Serial.begin(115200); //Sets the data transfer rate at 115.2 Kbps
+  Serial1.begin(9600);
   Serial.print("\n\n\nready\n\n\n\n\n\n");
+  ET.begin(details(dataTX),&Serial1);
 }
 
 void loop()
 {
-  sensor.getTotal(lineResult);
+  sensor.getTotal(dataTX.line);
   
   for(int i=0;i<16;i++)
   {
@@ -24,12 +33,12 @@ void loop()
   
   for(int i=0;i<16;i++)
   {
-    Serial.print(lineResult[i]);
+    Serial.print(dataTX.line[i]);
     Serial.print(", ");
   }
   
   Serial.println("");
   Serial.println("");
-  
+  ET.sendData();
   delay(1000);
 }
