1517e1638255b5b9abc0728831949bba1644981f
egaebel <egaebel@vt.edu>
2013-02-23 17:11:40 -0500

    added FULL state machine Test files

---
+++ ROBOT_2013/Sketchbook/StateMachineTest/States.h
@@ -0,0 +1,6 @@
+#ifndef STATES_H_
+#define STATES_H_H
+
+//We don't need anything here yet
+
+#endif
---
+++ ROBOT_2013/Sketchbook/StateMachineTest/common.h
@@ -0,0 +1,69 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+/*
+ * This is where all globals and constants should be defined
+*/
+//Enumerations
+enum POSITION{	
+	POS_START = 0,
+	POS_SEA,
+	POS_RAIL,
+	POS_LOAD,
+	POS_AIR
+};
+
+enum COLOR{
+    WHITE = 0,
+    BLUE = 1,
+    RED = 2,
+    GREEN = 3
+}; //TODO: There are still some colors to be defined, waiting on color sensor defs
+
+enum SIZE{
+	SMALL, 
+	MEDIUM, 
+	LARGE
+};
+
+//Structs
+POSITION curPos = POS_START;
+POSITION nextPos = NULL;
+
+typedef struct block_t {
+	COLOR color; 
+	SIZE size; 
+	bool present;
+} block;
+
+//Variables
+block lBlock; //Block held by the left claw.
+block rBlock; //Block held by the right claw.
+
+block loadingZone[14]; // Blocks in the loading zone, listed west to east.
+block seaZone[6]; //Sea zone colors, listed south to north.
+block railZone[6]; //Rail zone colors, listed west to east. 
+
+const int PICKUP_SIZE = 14;
+const int RAIL_SEA_SIZE = 6;
+const int AIR_SIZE = 2;
+
+/* PIN DEFINITIONS */
+	//Motors (Servos)
+#define LEFT_MOTOR 13
+#define RIGHT_MOTOR 12
+#define REAR_MOTOR 11 //Used for setSpeed
+#define REAR_MOTOR_L 11
+#define REAR_MOTOR_R 10
+	//Claw Servos
+#define LCLAW_SERVO 9
+#define RCLAW_SERVO 8
+#define LCLAW_EXTEND_SERVO 7
+#define RCLAW_EXTEND_SERVO 6
+	//Wallfolloer servos
+#define WALL_FOLLOW_SERVO 5
+#define BUMPER_2_PIN 4
+#define BUMPER_1_PIN 3
+#define COLOR_SENSOR 0 //Pin for color sensor (pin 0 is fake) 
+
+#endif
