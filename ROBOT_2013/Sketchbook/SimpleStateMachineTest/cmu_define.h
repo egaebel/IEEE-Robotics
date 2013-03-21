10e68cd65fc5f577cd2d01f550a67e67e18a21a9
Antoine Dahan <adahan@vt.edu>
2013-03-20 20:16:33 -0400

    Added color's RGB min/maxes to the cmu_define.h

--- ROBOT_2013/Sketchbook/SimpleStateMachineTest/cmu_define.h
+++ ROBOT_2013/Sketchbook/SimpleStateMachineTest/cmu_define.h
@@ -6,11 +6,49 @@
 #define WHITE_B_MAX 255
 
 #define BLUE_R_MIN 70
-#define BLUE_R_MAX 140
-#define BLUE_G_MIN 100
-#define BLUE_G_MAX 170
-#define BLUE_B_MIN 165
-#define BLUE_B_MAX 255
+#define BLUE_R_MAX 85
+#define BLUE_G_MIN 120
+#define BLUE_G_MAX 150
+#define BLUE_B_MIN 185
+#define BLUE_B_MAX 250
+
+#define RED_R_MIN 145
+#define RED_R_MAX 214
+#define RED_G_MIN 75
+#define RED_G_MAX 100
+#define RED_B_MIN 65
+#define RED_B_MAX 85
+
+#define YELLOW_R_MIN 125
+#define YELLOW_R_MAX 190
+#define YELLOW_G_MIN 235
+#define YELLOW_G_MAX 255
+#define YELLOW_B_MIN 235
+#define YELLOW_B_MAX 255
+
+#define BROWN_R_MIN 70
+#define BROWN_R_MAX 140
+#define BROWN_G_MIN 70
+#define BROWN_G_MAX 140
+#define BROWN_B_MIN 70
+#define BROWN_B_MAX 140
+
+#define GREEN_R_MIN 50
+#define GREEN_R_MAX 105
+#define GREEN_G_MIN 80
+#define GREEN_G_MAX 140
+#define GREEN_B_MIN 60
+#define GREEN_B_MAX 120
+
+#define PRUPLE_R_MIN 95
+#define PURPLE_R_MAX 130
+#define PRUPLE_G_MIN 110
+#define PURPLE_G_MAX 155
+#define PRUPLE_B_MIN 170
+#define PURPLE_B_MAX 220
+
+
+
 
 
 #define PIXEL_BLOCK_MIN 35 //minimum % of pixels to identify a block
