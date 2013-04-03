60d23f0cbe34df687d3f8bdff44a16ffc45d6b21
egaebel <egaebel@vt.edu>
2013-04-02 21:26:41 -0400

    color values updated

--- ROBOT_2013/Sketchbook/MainSource/cmu_define.h
+++ ROBOT_2013/Sketchbook/MainSource/cmu_define.h
@@ -1,51 +1,51 @@
-#define WHITE_R_MIN 170
-#define WHITE_R_MAX 255
-#define WHITE_G_MIN 170
+#define WHITE_R_MIN 225
+#define WHITE_R_MAX 235
+#define WHITE_G_MIN 250
 #define WHITE_G_MAX 255
-#define WHITE_B_MIN 205
+#define WHITE_B_MIN 250
 #define WHITE_B_MAX 255
 
-#define BLUE_R_MIN 80
-#define BLUE_R_MAX 110
-#define BLUE_G_MIN 110
-#define BLUE_G_MAX 165
-#define BLUE_B_MIN 180
-#define BLUE_B_MAX 245
+#define BLUE_R_MIN 120
+#define BLUE_R_MAX 180
+#define BLUE_G_MIN 250
+#define BLUE_G_MAX 255
+#define BLUE_B_MIN 250
+#define BLUE_B_MAX 255
 
-#define RED_R_MIN 150
-#define RED_R_MAX 220
-#define RED_G_MIN 70
-#define RED_G_MAX 160
-#define RED_B_MIN 65
-#define RED_B_MAX 135
+#define RED_R_MIN 205
+#define RED_R_MAX 255
+#define RED_G_MIN 105
+#define RED_G_MAX 120
+#define RED_B_MIN 90
+#define RED_B_MAX 110
 
-#define YELLOW_R_MIN 220
-#define YELLOW_R_MAX 270
-#define YELLOW_G_MIN 220
-#define YELLOW_G_MAX 270
-#define YELLOW_B_MIN 130
-#define YELLOW_B_MAX 180
+#define YELLOW_R_MIN 250
+#define YELLOW_R_MAX 255
+#define YELLOW_G_MIN 250
+#define YELLOW_G_MAX 255
+#define YELLOW_B_MIN 145
+#define YELLOW_B_MAX 160
 
-#define GREEN_R_MIN 40
-#define GREEN_R_MAX 55
-#define GREEN_G_MIN 60
-#define GREEN_G_MAX 85
-#define GREEN_B_MIN 35
-#define GREEN_B_MAX 60
+#define GREEN_R_MIN 55
+#define GREEN_R_MAX 75
+#define GREEN_G_MIN 90
+#define GREEN_G_MAX 115
+#define GREEN_B_MIN 70
+#define GREEN_B_MAX 115
 
-#define BROWN_R_MIN 55
-#define BROWN_R_MAX 65
-#define BROWN_G_MIN 56
-#define BROWN_G_MAX 66
-#define BROWN_B_MIN 49
-#define BROWN_B_MAX 59
+#define BROWN_R_MIN 65
+#define BROWN_R_MAX 75
+#define BROWN_G_MIN 65
+#define BROWN_G_MAX 75
+#define BROWN_B_MIN 55
+#define BROWN_B_MAX 70
 
-#define PURPLE_R_MIN 100
-#define PURPLE_R_MAX 145
-#define PURPLE_G_MIN 120
-#define PURPLE_G_MAX 165
-#define PURPLE_B_MIN 170
-#define PURPLE_B_MAX 215
+#define PURPLE_R_MIN 155
+#define PURPLE_R_MAX 175
+#define PURPLE_G_MIN 185
+#define PURPLE_G_MAX 200
+#define PURPLE_B_MIN 230
+#define PURPLE_B_MAX 255
 
 
 
