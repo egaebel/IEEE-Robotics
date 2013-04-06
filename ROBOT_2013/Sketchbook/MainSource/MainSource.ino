d1ebaacfefbaab4c7808858515109e61b24c4c34
Antoine Dahan <adahan@vt.edu>
2013-04-05 20:43:29 -0400

    def

--- ROBOT_2013/Sketchbook/MainSource/MainSource.ino
+++ ROBOT_2013/Sketchbook/MainSource/MainSource.ino
@@ -10,7 +10,9 @@
 #define DEBUG_FSM 0
 
 extern State initState;
-FiniteStateMachine fsm(initState);
+extern State moveToAirState;
+
+FiniteStateMachine fsm(moveToAirState);
 
 Sonar sonarLeft(SONAR_LEFT,SONAR_LEFT_INT);
 Sonar sonarRight(SONAR_RIGHT,SONAR_RIGHT_INT);
@@ -32,22 +34,23 @@ void handleSonarRight(){
 }
 
 void setup() {
-    
     delay(100); //Make sure we don't catch the PWM from POR of Sonar
-    attachInterrupt(SONAR_LEFT_INT,handleSonarLeft,FALLING);
-    attachInterrupt(SONAR_RIGHT_INT,handleSonarRight,FALLING);
+    //attachInterrupt(SONAR_LEFT_INT,handleSonarLeft,FALLING);
+    //attachInterrupt(SONAR_RIGHT_INT,handleSonarRight,FALLING);
 
-    Wire.begin();
+    //Wire.begin();
     delay(200);
 
 	Serial.begin(9600);
     Serial.println("SETUP COMPLETED!!");
+	
 }
 
 void loop() {
-    sonarLeft.update();
-    sonarRight.update();
-    Serial.println(sonarLeft.getDistance());
+	Serial.println("Got to loop");
+    //sonarLeft.update();
+    //sonarRight.update();
+    //Serial.println(sonarLeft.getDistance());
 #if DEBUG_FSM == 0
     fsm.update();
 #else
