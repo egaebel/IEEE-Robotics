f8923faca6671eb3efd0b4bb9f8cded178aec016
egaebel <egaebel@vt.edu>
2013-09-22 14:26:00 -0400

    added old CMU cam code to testing ground

---
+++ ROBOT_2014/CMUCamTesting/CMUTest/cmu_define.h
@@ -0,0 +1,113 @@
+//*******************BLOCK COLORS*********************************/
+
+#define WHITE_R_MIN 195
+#define WHITE_R_MAX 255
+#define WHITE_G_MIN 195
+#define WHITE_G_MAX 251
+#define WHITE_B_MIN 195
+#define WHITE_B_MAX 255
+
+#define BLUE_R_MIN 0
+#define BLUE_R_MAX 100
+#define BLUE_G_MIN 180
+#define BLUE_G_MAX 255
+#define BLUE_B_MIN 25
+#define BLUE_B_MAX 255
+
+#define RED_R_MIN 220
+#define RED_R_MAX 255
+#define RED_G_MIN 75
+#define RED_G_MAX 150
+#define RED_B_MIN 75
+#define RED_B_MAX 200
+
+#define YELLOW_R_MIN 200
+#define YELLOW_R_MAX 255
+#define YELLOW_G_MIN 200
+#define YELLOW_G_MAX 255
+#define YELLOW_B_MIN 150
+#define YELLOW_B_MAX 200
+
+#define GREEN_R_MIN 60
+#define GREEN_R_MAX 75//95
+#define GREEN_G_MIN 100
+#define GREEN_G_MAX 180
+#define GREEN_B_MIN 80
+#define GREEN_B_MAX 170
+
+#define BROWN_R_MIN 60
+#define BROWN_R_MAX 95
+#define BROWN_G_MIN 72
+#define BROWN_G_MAX 95
+#define BROWN_B_MIN 47
+#define BROWN_B_MAX 125
+
+//ORANGERED
+#define PURPLE_R_MIN 80
+#define PURPLE_R_MAX 150
+#define PURPLE_G_MIN 100
+#define PURPLE_G_MAX 200
+#define PURPLE_B_MIN 200
+#define PURPLE_B_MAX 245
+
+//*******************BAY COLORS*********************************/
+
+#define BAY_WHITE_R_MIN 195
+#define BAY_WHITE_R_MAX 255
+#define BAY_WHITE_G_MIN 195
+#define BAY_WHITE_G_MAX 251
+#define BAY_WHITE_B_MIN 195
+#define BAY_WHITE_B_MAX 255
+
+#define BAY_BLUE_R_MIN 0
+#define BAY_BLUE_R_MAX 100
+#define BAY_BLUE_G_MIN 180
+#define BAY_BLUE_G_MAX 255
+#define BAY_BLUE_B_MIN 25
+#define BAY_BLUE_B_MAX 255
+
+#define BAY_RED_R_MIN 90
+#define BAY_RED_R_MAX 255
+#define BAY_RED_G_MIN 115
+#define BAY_RED_G_MAX 175
+#define BAY_RED_B_MIN 90
+#define BAY_RED_B_MAX 165
+
+#define BAY_YELLOW_R_MIN 200
+#define BAY_YELLOW_R_MAX 255
+#define BAY_YELLOW_G_MIN 200
+#define BAY_YELLOW_G_MAX 255
+#define BAY_YELLOW_B_MIN 150
+#define BAY_YELLOW_B_MAX 255
+
+#define BAY_GREEN_R_MIN 33
+#define BAY_GREEN_R_MAX 87
+#define BAY_GREEN_G_MIN 80
+#define BAY_GREEN_G_MAX 155
+#define BAY_GREEN_B_MIN 55
+#define BAY_GREEN_B_MAX 165
+
+#define BAY_BROWN_R_MIN 60
+#define BAY_BROWN_R_MAX 95
+#define BAY_BROWN_G_MIN 72
+#define BAY_BROWN_G_MAX 95
+#define BAY_BROWN_B_MIN 47
+#define BAY_BROWN_B_MAX 125
+
+//ORANGERED
+#define BAY_PURPLE_R_MIN 80
+#define BAY_PURPLE_R_MAX 150
+#define BAY_PURPLE_G_MIN 100
+#define BAY_PURPLE_G_MAX 220
+#define BAY_PURPLE_B_MIN 150
+#define BAY_PURPLE_B_MAX 240
+//******************other constants
+
+#define CAM_BRIGHTNESS  0
+#define CAM_CONTRAST    25
+
+#define PIXEL_BLOCK_MIN 25 //minimum % of pixels to identify a block
+#define PIXEL_RAIL_MIN 25
+#define PIXEL_SEA_MIN 25
+#define PIXEL_BAY_MIN 30
+#define PIXEL_X_OFFSET 0 //change to position the claws better
