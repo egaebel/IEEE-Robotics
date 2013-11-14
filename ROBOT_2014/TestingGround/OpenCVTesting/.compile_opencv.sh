bb07b693859fb1d7219edc04a1f423e3c784bf07
David Butenhoff <eolxe@vt.edu>
2013-11-14 15:52:22 -0500

    added OpenCV debug compile script

---
+++ ROBOT_2014/TestingGround/OpenCVTesting/.compile_opencv.sh
@@ -0,0 +1,12 @@
+echo "compiling $1"
+if [[ $1 == *.c ]]
+then
+    gcc -DDEBUG -ggdb `pkg-config --cflags opencv` -o `basename $1 .c` $1 `pkg-config --libs opencv`;
+elif [[ $1 == *.cpp ]]
+then
+    g++ -DDEBUG -Wall -ggdb `pkg-config --cflags opencv` -o `basename $1 .cpp` $1 `pkg-config --libs opencv`;
+else
+    echo "Please compile only .c or .cpp files"
+fi
+echo "Output file => ${1%.*}"
+
