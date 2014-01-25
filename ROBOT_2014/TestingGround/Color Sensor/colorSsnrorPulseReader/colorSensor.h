46126cec1ad147d0ae5c05e80b8de6886a6d49ed
Antoine <adahan93@gmail.com>
2014-01-25 18:33:56 -0500

    Color sensor can detect Blue, Black, and White

---
+++ ROBOT_2014/TestingGround/Color Sensor/colorSsnrorPulseReader/colorSensor.h
@@ -0,0 +1,13 @@
+#ifndef BACKENDCOLORSENSOR_H
+#define BACKENDCOLORSENSOR_H
+#include <Arduino.h>
+class BackEndColorSensor
+{
+    public:
+        BackEndColorSensor(); //Nescessary color sensor setup called in constructor
+        int colorRead(int color);
+    private:
+        void TCS3200setup();
+};
+
+#endif // BACKENDCOLORSENSOR_H
