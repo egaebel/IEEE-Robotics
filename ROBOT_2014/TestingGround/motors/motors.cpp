ccf5fca104505e07964446711ab23e2ee5d20af9
Zach Adam <zachadam@vt.edu>
2014-01-25 12:14:16 -0500

    motors

---
+++ ROBOT_2014/TestingGround/motors/motors.cpp
@@ -0,0 +1,100 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+#include "motors.h"
+void motors::setup()
+{
+//Set the PIN Modes
+  pinMode(pinPWMA, OUTPUT);
+  pinMode(pinAIN1, OUTPUT);
+  pinMode(pinAIN2, OUTPUT);
+
+  pinMode(pinPWMB, OUTPUT);
+  pinMode(pinBIN1, OUTPUT);
+  pinMode(pinBIN2, OUTPUT);
+
+  pinMode(pinSTBY, OUTPUT);
+
+}
+
+
+
+void motors::motorDrive(boolean motorNumber, boolean motorDirection)
+{
+  /*
+  This Drives a specified motor, in a specific direction, at a specified speed:
+    - motorNumber: motor1 or motor2 ---> Motor 1 or Motor 2
+    - motorDirection: turnCW or turnCCW ---> clockwise or counter-clockwise
+    - motorSpeed: 0 to 255 ---> 0 = stop / 255 = fast
+  */
+
+  boolean pinIn1;  //Relates to AIN1 or BIN1 (depending on the motor number specified)
+
+ 
+//Specify the Direction to turn the motor
+  //Clockwise: AIN1/BIN1 = HIGH and AIN2/BIN2 = LOW
+  //Counter-Clockwise: AIN1/BIN1 = LOW and AIN2/BIN2 = HIGH
+  if (motorDirection == turnCW)
+    pinIn1 = HIGH;
+  else
+    pinIn1 = LOW;
+
+//Select the motor to turn, and set the direction and the speed
+  if(motorNumber == motor1)
+  {
+    digitalWrite(pinAIN1, pinIn1);
+    digitalWrite(pinAIN2, !pinIn1);  //This is the opposite of the AIN1
+    analogWrite(pinPWMA, speed);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, pinIn1);
+    digitalWrite(pinBIN2, !pinIn1);  //This is the opposite of the BIN1
+    analogWrite(pinPWMB, speed);
+  }
+   
+ 
+
+//Finally , make sure STBY is disabled - pull it HIGH
+  digitalWrite(pinSTBY, HIGH);
+
+}
+
+void motors::motorBrake(boolean motorNumber)
+{
+/*
+This "Short Brake"s the specified motor, by setting speed to zero
+*/
+
+  if (motorNumber == motor1)
+    analogWrite(pinPWMA, 0);
+  else
+    analogWrite(pinPWMB, 0);
+   
+}
+
+
+void motors::motorStop(boolean motorNumber)
+{
+  /*
+  This stops the specified motor by setting both IN pins to LOW
+  */
+  if (motorNumber == motor1) {
+    digitalWrite(pinAIN1, LOW);
+    digitalWrite(pinAIN2, LOW);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, LOW);
+    digitalWrite(pinBIN2, LOW);
+  } 
+}
+
+
+void motors::motorsStandby()
+{
+  /*
+  This puts the motors into Standby Mode
+  */
+  digitalWrite(pinSTBY, LOW);
+}
+#endif
