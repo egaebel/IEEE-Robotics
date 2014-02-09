315a7b0805ffba243c09293560112418d6321438
egaebel <egaebel@vt.edu>
2014-02-09 15:52:50 -0500

    working motor code\! As well as updated rules under Documentation

--- ROBOT_2014/TestingGround/motors1/motors.cpp
+++ ROBOT_2014/TestingGround/motors1/motors.cpp
@@ -1,98 +1,109 @@
-
 #include "motors.h"
-void motors::setup()
+
+//Sets up the pin modes and the default speed.
+void motors::setup(int defaultSpeed)
 {
-//Set the PIN Modes
-  pinMode(pinPWMA, OUTPUT);
-  pinMode(pinAIN1, OUTPUT);
-  pinMode(pinAIN2, OUTPUT);
+    delay(2);
 
-  pinMode(pinPWMB, OUTPUT);
-  pinMode(pinBIN1, OUTPUT);
-  pinMode(pinBIN2, OUTPUT);
+    //Set the PIN Modes
+    pinMode(PIN_PWM_LEFT, OUTPUT);
+    pinMode(PIN_DIRECTION_LEFT, OUTPUT);
 
-  pinMode(pinSTBY, OUTPUT);
+    pinMode(PIN_PWM_RIGHT, OUTPUT);
+    pinMode(PIN_DIRECTION_RIGHT, OUTPUT);
 
+    speed = defaultSpeed;
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 }
 
-
-
-void motors::motorDrive(bool motorNumber, bool motorDirection)
-{
-  /*
-  This Drives a specified motor, in a specific direction, at a specified speed:
-    - motorNumber: motor1 or motor2 ---> Motor 1 or Motor 2
-    - motorDirection: turnCW or turnCCW ---> clockwise or counter-clockwise
-    - motorSpeed: 0 to 255 ---> 0 = stop / 255 = fast
-  */
-
-  bool pinIn1;  //Relates to AIN1 or BIN1 (depending on the motor number specified)
-
- 
-//Specify the Direction to turn the motor
-  //Clockwise: AIN1/BIN1 = HIGH and AIN2/BIN2 = LOW
-  //Counter-Clockwise: AIN1/BIN1 = LOW and AIN2/BIN2 = HIGH
-  if (motorDirection == turnCW)
-    pinIn1 = HIGH;
-  else
-    pinIn1 = LOW;
-
-//Select the motor to turn, and set the direction and the speed
-  if(motorNumber == motor1)
-  {
-    digitalWrite(pinAIN1, pinIn1);
-    digitalWrite(pinAIN2, !pinIn1);  //This is the opposite of the AIN1
-    analogWrite(pinPWMA, speed);
-  }
-  else
-  {
-    digitalWrite(pinBIN1, pinIn1);
-    digitalWrite(pinBIN2, !pinIn1);  //This is the opposite of the BIN1
-    analogWrite(pinPWMB, speed);
-  }
-   
- 
-
-//Finally , make sure STBY is disabled - pull it HIGH
-  digitalWrite(pinSTBY, HIGH);
-
-}
-
-void motors::motorBrake(bool motorNumber)
-{
 /*
-This "Short Brake"s the specified motor, by setting speed to zero
-*/
+ * Drives the motors in the direction passed in. The Enum Direction has FORWARD, and BACKWARD as values.
+ *
+ */
+void motors::motorsDrive(Direction motorsDirection) 
+{
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 
-  if (motorNumber == motor1)
-    analogWrite(pinPWMA, 0);
-  else
-    analogWrite(pinPWMB, 0);
-   
+    bool pinInput;
+ 
+    if (motorsDirection == FORWARD) {
+        pinInput = HIGH;
+        Serial.write("pinInput is HIGH!\n");
+    }
+    else {
+        pinInput = LOW;
+        Serial.write("pinInput is LOW!\n");
+    }
+
+    digitalWrite(PIN_DIRECTION_LEFT, pinInput);
+    analogWrite(PIN_PWM_LEFT, speed);   
+
+    digitalWrite(PIN_DIRECTION_RIGHT, pinInput);
+    analogWrite(PIN_PWM_RIGHT, speed);
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 }
 
+//Turns the robot left.
+void motors::motorsTurnLeft() {
+    
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 
-void motors::motorStop(bool motorNumber)
-{
-  /*
-  This stops the specified motor by setting both IN pins to LOW
-  */
-  if (motorNumber == motor1) {
-    digitalWrite(pinAIN1, LOW);
-    digitalWrite(pinAIN2, LOW);
-  }
-  else
-  {
-    digitalWrite(pinBIN1, LOW);
-    digitalWrite(pinBIN2, LOW);
-  } 
+    motorsTurn(LEFT);
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 }
 
+//Turns the robot right.
+void motors::motorsTurnRight() {
+    
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 
-void motors::motorsStandby()
-{
-  /*
-  This puts the motors into Standby Mode
-  */
-  digitalWrite(pinSTBY, LOW);
+    motorsTurn(RIGHT);
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
+}
+
+//Turns the motors in the specified direction (used to make the above methods work, not to be interfaced with)
+void motors::motorsTurn(Turn motorsTurn){
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
+
+    bool pinInput;
+
+    if (motorsTurn == LEFT) 
+        pinInput = LOW;
+    else
+        pinInput = HIGH;
+
+    digitalWrite(PIN_DIRECTION_LEFT, pinInput);
+    analogWrite(PIN_PWM_LEFT, speed);   
+
+    digitalWrite(PIN_DIRECTION_RIGHT, !pinInput);
+    analogWrite(PIN_PWM_RIGHT, speed);   
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
+}
+
+//Cuts power to the motors, roll to a stop
+void motors::motorsBrake() {
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
+
+    analogWrite(PIN_PWM_LEFT, 0);  
+    analogWrite(PIN_PWM_RIGHT, 0);  
+
+    //Delay 2 milliseconds so we don't explode....
+    delay(2);
 }
