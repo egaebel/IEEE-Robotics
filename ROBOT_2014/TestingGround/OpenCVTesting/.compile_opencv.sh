533c8068cf749c36f8962df321a1581fa8ea4823
dbutenhoff <eolxe@vt.edu>
2014-02-12 23:40:01 -0500

    fixed compile flags in opencv compile script

--- ROBOT_2014/TestingGround/OpenCVTesting/.compile_opencv.sh
+++ ROBOT_2014/TestingGround/OpenCVTesting/.compile_opencv.sh
@@ -1,10 +1,10 @@
 echo "compiling $1"
 if [[ $1 == *.c ]]
 then
-    gcc -DDEBUG -Wall -march=armv7-a -mtune=cortex-a8 -ffast-math -O3 -mfpu=neon -mfloat-abi=hard -ggdb `pkg-config --cflags opencv` -o `basename $1 .c` $1 `pkg-config --libs opencv`;
+    gcc -DDEBUG -Wall -march=armv7-a -mtune=cortex-a8 -ffast-math -O3 -mfpu=neon -mfloat-abi=hard -ggdb `pkg-config --cflags opencv` -o bin/`basename $1 .c` $1 `pkg-config --libs opencv`;
 elif [[ $1 == *.cpp ]]
 then
-    g++ -DDEBUG -Wall -march=armv7-a -mtune=cortex-a8 -ffast-math -O3 -mfpu=neon -mfloat-abi=hard -ggdb `pkg-config --cflags opencv` -o `basename $1 .cpp` $1 `pkg-config --libs opencv`;
+    g++ -DDEBUG -Wall -march=armv7-a -mtune=cortex-a8 -ffast-math -O3 -mfpu=neon -mfloat-abi=hard -ggdb `pkg-config --cflags opencv` -o bin/`basename $1 .cpp` $1 `pkg-config --libs opencv`;
 else
     echo "Please compile only .c or .cpp files"
 fi
