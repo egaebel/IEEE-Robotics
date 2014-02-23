90d25b83321bc9489f608347047a771cc54a0839
egaebel <egaebel@vt.edu>
2014-02-23 15:10:36 -0500

    added pins.h file

---
+++ ROBOT_2014/TestingGround/demo/pins.h
@@ -0,0 +1,14 @@
+#ifndef PIN_DEFINITIONS_H_
+
+//Pin assignments
+
+//Motor Pair 1 (ON THE LEFT SIDE)
+#define PIN_DIRECTION_LEFT 8 //Direction
+#define PIN_PWM_LEFT 3 //Speed
+//Motor Pair 2 (ON THE RIGHT SIDE)
+#define PIN_DIRECTION_RIGHT 9 //Direction
+#define PIN_PWM_RIGHT 4 //Speed
+
+//Line Sensor Pins
+#define Load = 22; // Controls ShiftRegister's Shift/Load pin(1)
+#define sensor = 24; // Controls LineFollower's Enable pin
