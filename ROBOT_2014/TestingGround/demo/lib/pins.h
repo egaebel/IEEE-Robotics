24dc39fc922d186ffdb32ffde4e52b5320aa4e8c
egaebel <egaebel@vt.edu>
2014-03-02 16:26:34 -0500

    fixed pin defs...oops

--- ROBOT_2014/TestingGround/demo/lib/pins.h
+++ ROBOT_2014/TestingGround/demo/lib/pins.h
@@ -1,8 +1,7 @@
 #ifndef PIN_DEFINITIONS_H_
 #define PIN_DEFINITIONS_H_
 
-//Pin assignments--------------------------
-
+//Motor Pins---------------------------------------
 //Motor Pair 1 (ON THE LEFT SIDE)
 static const unsigned short PIN_DIRECTION_LEFT = 26; //Direction
 static const unsigned short PIN_PWM_LEFT = 3; //Speed
@@ -11,8 +10,16 @@ static const unsigned short PIN_DIRECTION_RIGHT = 28; //Direction
 static const unsigned short PIN_PWM_RIGHT = 4; //Speed
 static const unsigned int DEFAULT_SPEED = 255;
 
-//Line Sensor Pins
+//Line Sensor Pins--------------------------------
 static const unsigned short PIN_LOAD = 22; // Controls ShiftRegister's Shift/Load pin(1)
 static const unsigned short PIN_SENSOR = 24; // Controls LineFollower's Enable pin
+
+//Color Sensor Pins-------------------------------
+static const unsigned short CS_S0 = 31;
+static const unsigned short CS_S1 = 33;
+static const unsigned short CS_S2 = 37;
+static const unsigned short CS_S3 = 39;
+static const unsigned short CS_OUT = 41;
+static const unsigned short CS_LED = 43;
 
 #endif
