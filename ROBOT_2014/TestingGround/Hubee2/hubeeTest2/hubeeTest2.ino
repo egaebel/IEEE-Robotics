e8b4b7a9f68c78a00393902d18691413a91005a0
egaebel <egaebel@vt.edu>
2014-03-10 11:31:32 -0400

    hubee changes, very simple interface now

--- ROBOT_2014/TestingGround/Hubee2/hubeeTest2/hubeeTest2.ino
+++ ROBOT_2014/TestingGround/Hubee2/hubeeTest2/hubeeTest2.ino
@@ -3,21 +3,25 @@
  * James Reed, CpE
  * Created 2/9/2014
  *
- * This file contains a working test for the HUBeeBMDWheel class.
+ * This file contains a working test for the HUBeeBMDwheel class.
  */
 
-#include <HUBeeBMDWheel.h>
+#include "Hubee.h"
+#include <Arduino.h>
 //****************Pin Assignments*****************
 // HuBee wheel reference: http://www.creative-robotics.com/bmdsresources
 // 
 // Pin IN1 on hubee wheel. Digital pin 22
-const int PIN_IN1 = 22;
+static const unsigned int PIN_IN1 = 22;
 //
 // Pin IN2 on hubee wheel. Digital pin 24
-const int PIN_IN2 = 24;
+static const unsigned int PIN_IN2 = 24;
 //
 // PWM pin on hubee wheel. PWM pin 9
-const int PIN_PWM = 9;
+static const unsigned int PIN_PWM = 9;
+
+//Standby pin on hubee
+static const unsigned int PIN_STANDBY = 26;
 
 //***************Wire assignments ***************
 //
@@ -29,34 +33,39 @@ const int PIN_PWM = 9;
 // GREEN: Standby (active low - can be left disconnected)
 // YELLOW/ORANGE: Quadrature sensor channels A and B (not used in this)
 //
-HUBeeBMDWheel Wheel; // create our wheel object
+Hubee wheel; // create our wheel object
 
 int Speed = 255; // static speed for testing purposes
 
-signed char timeout= -255; // demonstration timeout
+signed int timeout= -254; // demonstration timeout
 
 void setup()
 {
-  //setup our pins
-  // setupPins(int In1Pin, int In2Pin, int PWMPin);
-  Wheel.setupPins(PIN_IN1, PIN_IN2, PIN_PWM);
-  // set direction mode to true to invert normal direction
-  Wheel.setDirectionMode(false);
-  
+
   Serial.begin(9600);
   
-  // set motor power with signed integer -255 to 255
-  Wheel.setMotorPower(Speed);
+  //setup our pins and motor Power
+  wheel.setup(PIN_IN1, PIN_IN2, PIN_PWM, 255);
 }
 
 void loop()
 {
    Serial.print("loop()\n");
-   Serial.print(Wheel.getMotorPower());
+   //Serial.print(wheel.getMotorPower());
    Serial.print("\n");
-   
-   //Drive wheels. Using timeout variable to switch direction
-   Wheel.setMotorPower((timeout++ < 0) ? Speed : -Speed);
 
-   delay(1); 
+   //Definitely works...
+   //digitalWrite(PIN_IN1, LOW);
+   //digitalWrite(PIN_IN2, HIGH);
+   //analogWrite(PIN_PWM, 255);
+   //------------------------------
+   
+   wheel.forward();
+   delay(4000);
+   wheel.stop();
+   delay(1000);
+   wheel.backward();
+   delay(4000);
+   wheel.stop();
+   delay(1000);
 }
