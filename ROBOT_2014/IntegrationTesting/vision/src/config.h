a82aaf788931eed260a662e7408581505e66459b
egaebel <egaebel@vt.edu>
2014-03-11 20:29:44 -0400

    with a red filter\!

--- ROBOT_2014/IntegrationTesting/vision/src/config.h
+++ ROBOT_2014/IntegrationTesting/vision/src/config.h
@@ -13,14 +13,14 @@
 // FYI: hue ranges from 0 to 179 in OpenCV
 #define MIN_HUE			0
 #define LOWER_HUE		0
-#define UPPER_HUE		10
+#define UPPER_HUE		1
 #define MAX_HUE			179
 // FYI: saturation ranges from 0 to 255 in OpenCV
-#define LOWER_SAT		76
-#define UPPER_SAT		178
+#define LOWER_SAT		229
+#define UPPER_SAT		255
 // FYI: value ranges from 0 to 255 in OpenCV
-#define LOWER_VAL		76
-#define UPPER_VAL		127
+#define LOWER_VAL		102
+#define UPPER_VAL		178
 
 // hull size filter range
 // one corner of the target at furthest distance and 320x240 is about 180 square pixels
