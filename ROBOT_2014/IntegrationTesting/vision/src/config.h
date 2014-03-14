126acfffd2a15b2a0f18a94aba02e4ee52429c8c
egaebel <egaebel@vt.edu>
2014-03-14 14:31:22 -0400

    room hue values with light

--- ROBOT_2014/IntegrationTesting/vision/src/config.h
+++ ROBOT_2014/IntegrationTesting/vision/src/config.h
@@ -12,15 +12,15 @@
 
 // FYI: hue ranges from 0 to 179 in OpenCV
 #define MIN_HUE			0
-#define LOWER_HUE		160//150
-#define UPPER_HUE		179//160
+#define LOWER_HUE		0//150
+#define UPPER_HUE		13//160
 #define MAX_HUE			179
 // FYI: saturation ranges from 0 to 255 in OpenCV
-#define LOWER_SAT		135//100
-#define UPPER_SAT		165//208
+#define LOWER_SAT		178//100
+#define UPPER_SAT		242//208
 // FYI: value ranges from 0 to 255 in OpenCV
-#define LOWER_VAL		68
-#define UPPER_VAL		132
+#define LOWER_VAL		165
+#define UPPER_VAL		219
 
 // hull size filter range
 // one corner of the target at furthest distance and 320x240 is about 180 square pixels
