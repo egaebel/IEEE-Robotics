52d43a863144ce41a9ea0431773d8fe05d251f39
egaebel <egaebel@vt.edu>
2014-03-15 02:55:06 -0400

    infinite aim

--- ROBOT_2014/IntegrationTesting/vision/src/hardware.h
+++ ROBOT_2014/IntegrationTesting/vision/src/hardware.h
@@ -1,7 +1,7 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
-bool write_hardware_fs( const char* path, const char* value, const int length, const char* mode = "rb+" );
+bool write_hardware_fs( const char* path, const char* value, const int length, const char* mode = "wb+" );
 
 bool read_hardware_fs( const char* path, char* value, unsigned int length, const char* mode );
 
--- ROBOT_2014/TestingGround/demo/demo.ino
+++ ROBOT_2014/TestingGround/demo/demo.ino
@@ -222,6 +222,8 @@ void loop() {
             //FIRE!---------------------------------------------------------------------------
             //Wait for beagle bone to aim
             
+            while (true || digitalWrite(START_AIMING_PIN, HIGH));
+
             startTime = millis();
             while (!fire/* && ((startTime + FIRE_WAIT_TIME) >= millis())*/);
 
