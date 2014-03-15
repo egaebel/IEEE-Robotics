00728777a9ac0cf2c976784eaadfb10dc019dcbc
egaebel <egaebel@vt.edu>
2014-03-15 07:13:34 -0400

    scaling with stuff in net

--- ROBOT_2014/IntegrationTesting/vision/src/config.h
+++ ROBOT_2014/IntegrationTesting/vision/src/config.h
@@ -13,14 +13,14 @@
 // FYI: hue ranges from 0 to 179 in OpenCV
 #define MIN_HUE			0
 #define LOWER_HUE		0//130
-#define UPPER_HUE		30//179
+#define UPPER_HUE		15//179
 #define MAX_HUE			179
 // FYI: saturation ranges from 0 to 255 in OpenCV
-#define LOWER_SAT		0
-#define UPPER_SAT		200
+#define LOWER_SAT		127
+#define UPPER_SAT		210
 // FYI: value ranges from 0 to 255 in OpenCV
-#define LOWER_VAL		0
-#define UPPER_VAL		200
+#define LOWER_VAL		120
+#define UPPER_VAL		185
 
 // hull size filter range
 // one corner of the target at furthest distance and 320x240 is about 180 square pixels
