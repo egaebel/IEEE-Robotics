59621d0c2b32a0bfb4577739c504306296a670f7
RusmanCool <rusmancool@yahoo.com>
2014-08-04 21:13:56 -0400

    For Deskon

---
+++ ROBOT_2014/IntegrationTesting/vision (1)/src/hardware.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file hardware
+ * @brief embedded hardware control via the filesystem
+ * @author D. Butenhoff
+ */
+
+#include <cstring>
+#include <cstdio>
+#include "hardware.h"
+
+bool write_hardware_fs( const char* path, const char* value, const int length, const char* mode )
+{
+  FILE *myOutputHandle = NULL;
+  if( ( myOutputHandle = fopen( path, mode ) ) == NULL ) {
+    printf( "Unable to open %s\n", path );
+    return false;
+  }
+  fwrite( value, sizeof(char), length, myOutputHandle );
+  fclose( myOutputHandle );
+  return true;
+}
+
+bool read_hardware_fs( const char* path, char* value, unsigned int length, const char* mode )
+{
+  FILE *myInputHandle = NULL;
+  if( ( myInputHandle = fopen( path, mode ) ) == NULL ) {
+    printf( "Unable to open %s\n", path );
+    return false;
+  }
+  bool failed = false;
+  int bytesRead = 0;
+  if ( (bytesRead = fread( value, sizeof(char), length, myInputHandle )) != length ) {
+    failed = true;
+    printf("failed to read byte from file %s, read %d bytes\n", path, bytesRead);
+  }
+  fclose( myInputHandle );
+  return !failed;
+}
---
+++ ROBOT_2014/IntegrationTesting/vision (1)/src/hardware.h
@@ -0,0 +1,8 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+bool write_hardware_fs( const char* path, const char* value, const int length, const char* mode = "wb+" );
+
+bool read_hardware_fs( const char* path, char* value, unsigned int length, const char* mode );
+
+#endif
